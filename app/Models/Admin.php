4f7080bcfeace89276addd3f61c53972a607c627
mishima2t73 <shima.k7646@gmail.com>
2021-06-18 17:27:36 +0900

    統一

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Admin extends Authenticatable
 {
