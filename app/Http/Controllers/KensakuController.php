ae01c3e834a8925d51aa753110473ec24d26742b
A1220012\kuroda_sh <kuroda_sh@atmarksol.jp>
2021-06-24 11:30:37 +0900

    検索controller

---
+++ app/Http/Controllers/KensakuController.php
@@ -0,0 +1,28 @@
+﻿<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\kensaku;
+ 
+class KensakuController extends Controller
+{
+    public function index(Request $request)
+    {
+        $keyword = $request->input('keyword');
+        $stock = $request->input('stock');
+ 
+        $query = Kensaku::query();
+ 
+        if (!empty($keyword)) {
+            $query->where('title', 'LIKE', "%{$keyword}%")
+                ->orWhere('author', 'LIKE', "%{$keyword}%");
+        }
+ 
+        if (!empty($stock)) {
+            $query->where('stock', '>=', $stock);
+        }
+ 
+        $kensaku = $query->get();
+ 
+        return view('kensaku.index', compact('kensaku', 'keyword', 'stock'));
+    }
+}
